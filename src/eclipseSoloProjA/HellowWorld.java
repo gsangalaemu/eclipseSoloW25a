55d4275a8aa1c0c46f1f582898dff18e62a7271c
emu <emu@164.76.205.206>
2025-02-03 11:51:57 -0500

    Added a 2nd println

--- src/eclipseSoloProjA/HellowWorld.java
+++ src/eclipseSoloProjA/HellowWorld.java
@@ -3,5 +3,6 @@ package eclipseSoloProjA;
 public class HellowWorld {
 	public static void main(String args[]) {
 		System.out.println("Hello world");
+		System.out.println("Hello world line 2");
 	}
 }
