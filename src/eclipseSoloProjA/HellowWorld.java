0801d57c524dfd9aee4ab621fc9b247585918528
emu <emu@164.76.205.206>
2025-02-03 12:25:28 -0500

    Step 6.2 -- Added line to Hello world

--- src/eclipseSoloProjA/HellowWorld.java
+++ src/eclipseSoloProjA/HellowWorld.java
@@ -4,5 +4,6 @@ public class HellowWorld {
 	public static void main(String args[]) {
 		System.out.println("Hello world");
 		System.out.println("Hello world line 2 edit 2");
+		System.out.println("Programmer2 did this.");
 	}
 }
