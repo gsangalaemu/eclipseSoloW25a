6ae76da72cb4c3a32b55ca512f376a22b9389722
gsangalaemu <gsangala@emich.edu>
2025-02-06 16:12:23 -0500

    Have to revert changes, did something wrong and the push went through when it should have failed

--- src/eclipseSoloProjA/HellowWorld.java
+++ src/eclipseSoloProjA/HellowWorld.java
@@ -4,6 +4,6 @@ public class HellowWorld {
 	public static void main(String args[]) {
 		System.out.println("Hello world");
 		System.out.println("Hello world line 2 edit 2");
-		System.out.println("Programmer2 did this. - ProgrammerOne did this.");
+		System.out.println("Programmer2 did this. - Programmer1 did this.");
 	}
 }
